d9aba90bdb7aa7797561629013bd7fec1dda6aa9
Ansh Tyagi <anshtyagi2222@gmail.com>
2023-06-03 21:00:14 +0530

    bg image auth

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image"
 
 type AuthPageProps = {}
 
@@ -7,6 +8,10 @@ const AuthPage:React.FC<AuthPageProps> = () => {
     return <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
         <div className="max-w-7xl mx-auto"></div>
         <Navbar/>
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+            <Image src="/hero.png" alt="Hero PNG" height="0" width="700"/>
+        </div>
+
     </div>
 }
 export default AuthPage
