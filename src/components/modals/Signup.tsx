6b72a488b3ab0b266160e1936b57ab511231d322
Ansh Tyagi <ansh.tyagi@mylofamily.com>
2023-06-25 13:38:43 +0530

    Signup auth with firebase

--- src/components/modals/Signup.tsx
+++ src/components/modals/Signup.tsx
@@ -1,6 +1,9 @@
-import React from "react"
+import React, {useState} from "react"
 import {useSetRecoilState} from "recoil";
 import {authModalState} from "@/atoms/authModalAtom";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {auth} from "@/firebase/firebase"
+import {useRouter} from "next/router";
 
 type SignupProps = {}
 
@@ -9,27 +12,51 @@ const Signup:React.FC<SignupProps> = () => {
     const handleClick = (type: "login" | "register" | "forgotPassword") => {
         setAuthModalState((prev)=> ({...prev,type:type}))
     }
-    return <form className="space-y-6 px-6 py-4">
+    const [inputs,setInputs] = useState({email:'',displayName:'',password:''})
+    const router = useRouter()
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const handleChangeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev)=>({...prev,[e.target.name]:e.target.value}))
+    }
+    const handleRegister = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
+            console.log(newUser)
+            if (!newUser) return
+            router.push("/")
+        }
+        catch (error:any) {
+            alert(error.message)
+        }
+
+    }
+    return <form className="space-y-6 px-6 py-4"  onSubmit={handleRegister}>
         <h3 className="text-xl font-medium text-white">Register to CodeNub</h3>
         <div>
             <label htmlFor="email" className="text-sm font-medium block mb-2 text-gray-300">
                 Email
             </label>
-            <input type="email" name="email" id="email" className="border-2 outline-none sm:text-sm rounded-lg focus:border-blue-500 block w-full p-2.5
+            <input onChange={handleChangeInput} type="email" name="email" id="email" className="border-2 outline-none sm:text-sm rounded-lg focus:border-blue-500 block w-full p-2.5
             bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder="name@company.com"/>
         </div>
         <div>
             <label htmlFor="displayName" className="text-sm font-medium block mb-2 text-gray-300">
                 Display Name
             </label>
-            <input type="displayName" name="displayName" id="displayName" className="border-2 outline-none sm:text-sm rounded-lg focus:border-blue-500 block w-full p-2.5
+            <input onChange={handleChangeInput} type="displayName" name="displayName" id="displayName" className="border-2 outline-none sm:text-sm rounded-lg focus:border-blue-500 block w-full p-2.5
             bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder="Elon Musk"/>
         </div>
         <div>
             <label htmlFor="password" className="text-sm font-medium block mb-2 text-gray-300">
                 Password
             </label>
-            <input type="password" name="password" id="password" className="border-2 outline-none sm:text-sm rounded-lg focus:border-blue-500 block w-full p-2.5
+            <input onChange={handleChangeInput} type="password" name="password" id="password" className="border-2 outline-none sm:text-sm rounded-lg focus:border-blue-500 block w-full p-2.5
             bg-gray-600 border-gray-500 placeholder-gray-400 text-white" placeholder="********"/>
         </div>
         <button type="submit" className="w-full text-white focus:ring-blue-300 font-medium rounded-lg
