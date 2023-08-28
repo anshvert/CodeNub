15fa63ecc748809892bf2a74b67c5f2958dd0d43
Ansh Tyagi <ansh.tyagi@mylofamily.com>
2023-08-28 11:29:17 +0530

    removed Old logo

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -43,9 +43,6 @@ const Topbar:React.FC<TopbarProps> = ({problemPage}) => {
         <>
             <nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
                 <div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto":""}`}>
-                    <Link href='/' className='h-[22px] flex-1'>
-                        <Image src='/logo-full.png' alt='Logo' className='h-full' width="90" height="100" />
-                    </Link>
                     {problemPage && (
                         <div className="flex items-center gap-4 flex-1 justify-center">
                             <div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer" onClick={()=>handleProblemChange(false)}>
